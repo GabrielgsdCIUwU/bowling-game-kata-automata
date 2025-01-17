1720faabbca18f56b6e508573f5cb1b681581a18
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-01-17 13:58:33 +0100

    test(symbol_zero): Añadir nuevo caso test de símbolo 0

--- test/test_symbol_zero.py
+++ test/test_symbol_zero.py
@@ -4,3 +4,8 @@ def test_symbol_zero():
     total = 90
     score_card = ScoreCard(pins)
     assert score_card.get_score() == total
+
+    pins = "9-3561368153258-7181"
+    total = 82
+    score_card = ScoreCard(pins)
+    assert score_card.get_score() == total
