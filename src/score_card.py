11721de299bcadc1c447cc7221b5dac4c09cc4fb
ccastroto <castroaldrovercarlossangrinha@gmail.com>
2025-01-17 13:31:49 +0100

    feat(scored_card): añadir la idea de la funcion

--- src/score_card.py
+++ src/score_card.py
@@ -4,3 +4,18 @@ class ScoreCard:
         
     def get_pins(self):
         return self.pins
+    
+    def get_score(self):
+        
+        
+        return
+    
+    def __pins_to_list(self):
+        all_frames = []
+        frame = []
+        for pin in self.pins:
+            frame.append(pin)
+            if len(frame) == 2:
+                all_frames.append(frame)
+                frame = []
+        return all_frames
