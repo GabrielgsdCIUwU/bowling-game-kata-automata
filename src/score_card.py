51b1dc2c1ed3237ce4a2ae804a53d3e5469741d0
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-01-20 14:26:20 +0100

    refactor(score_card): refactorizar comprobación de pins a una función

--- src/score_card.py
+++ src/score_card.py
@@ -6,26 +6,27 @@ class ScoreCard:
         return self.pins
     
     def get_score(self):
-        all_frames = self.__pins_to_list()
-
+        all_frames = self.__pins_to_frames()
         total_score = 0
         for frame in all_frames:
             total_score += sum(frame)
         
         return total_score
     
-    def __pins_to_list(self):
+    def __pins_to_frames(self):
         all_frames = []
         frame = []
-        for pin in self.pins:
-            if pin not in ['/', '-']:
-                frame.append(int(pin))
-            elif pin == '-':
-                frame.append(0)
-            else:
-                frame.append(pin)
-
+        for pin in self.get_pins():
+            frame.append(self.__pin_to_value(pin))
             if len(frame) == 2:
                 all_frames.append(frame)
                 frame = []
         return all_frames
+
+    def __pin_to_value(self, pin):
+        if pin not in ['/', '-']:
+            return int(pin)
+        elif pin == '-':
+            return 0
+        else:
+            return pin
