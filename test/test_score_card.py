66703bb2802c352a375dab1f040ff72cf679a70d
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-01-20 10:11:32 +0100

    feat(marker): Añadir markers en los casos test y el fichero pytest.ini

--- test/test_score_card.py
+++ test/test_score_card.py
@@ -1,18 +1,24 @@
 from src.score_card import ScoreCard
+import pytest
+
+@pytest.mark.score_card
 def test_score_card():
     assert ScoreCard
 
+@pytest.mark.get_pins
 def test_get_pins():
     PINS = "12345123451234512345"
     score_card = ScoreCard(PINS)
     assert score_card.get_pins() == PINS
 
+@pytest.mark.regular_pins
 def test_hitting_pins_regular():
     pins = "12345123451234512345"
     total = 60
     score_card = ScoreCard(pins)
     assert score_card.get_score() == total
 
+@pytest.mark.symbol_zero
 def test_symbol_zero():
     pins = "9-9-9-9-9-9-9-9-9-9-"
     total = 90
@@ -24,6 +30,7 @@ def test_symbol_zero():
     score_card = ScoreCard(pins)
     assert score_card.get_score() == total
 
+@pytest.mark.spare_not_extra
 def test_spare_not_extra():
     pins = "9-3/613/815/-/8-7/8-"
     total = 121
