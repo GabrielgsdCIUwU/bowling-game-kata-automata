abb0391009407e90538a998fb598461c556494ec
ccastroto <castroaldrovercarlossangrinha@gmail.com>
2025-01-20 15:10:32 +0100

    test(scored_card): Añadir caso test del strike

--- test/test_score_card.py
+++ test/test_score_card.py
@@ -43,3 +43,8 @@ def test_strike():
     total = 100
     score_card = ScoreCard(pins)
     assert score_card.get_score() == total
+
+    pins = "X9-X9-9-9-9-9-9-9-"
+    total = 110
+    score_card = ScoreCard(pins)
+    assert score_card.get_score() == total
